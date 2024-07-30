a090ddcf6a9dd9b46a7a5b6ce5123da22ceba11f
TRAN HAI NAM <toilatranhainam@gmail.com>
2024-07-30 11:06:32 +0700

    Update web

--- README.md
+++
@@ -1,9 +0,0 @@
-# Landing page của Thành, Nam, Long
-- Tải code về và tận hưởng với Live server thôi
-
-- Page in beta! (98%)
-
-- Fork with [Replit](https://replit.com/@HaiDarkCSS/Landing-page)
-
-> [!WARNING]  
-> Bản quyền thuộc về Rất Dev (Dev-nguvl-1803), nếu mượn code hãy trả 10$ 🐧
--- core.js
+++ core.js
@@ -1,6 +1,4 @@
 function showVideo(mode) {
-
-
     // Hide all videos
     document.getElementById('vo-dai-video').style.display = 'none';
     document.getElementById('summoners-rift-video').style.display = 'none';
@@ -15,19 +13,53 @@ function showVideo(mode) {
 function addClass(element, className) {
     var buttons = document.getElementsByClassName('game-mode-button');
     for (var i = 0; i < buttons.length; i++) {
-        console.log(`CHECK BUTTONS: ${buttons[i].classList}`)
         buttons[i].classList.remove(className);
     }
 
     element.classList.add(className);
 }
 
+function theme(element, text) {
+    var cards = document.getElementsByClassName(text);
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].classList.toggle('dark');
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     const searchIcon = document.querySelector('.search-icon');
     const searchBox = document.querySelector('.search-box');
 
-    searchIcon.addEventListener('mouseenter', () => {
+    searchIcon.addEventListener('click', () => {
         searchBox.focus();
     });
 });
+
+function myToggle() {
+    // Mode
+    var element = document.body;
+    var gameMode = document.getElementById('gameplay')
+    var newsSection = document.getElementById('news')
+    var section1 = document.getElementById('introduction')
+    var learnMore = document.getElementById('learn-more-btw')
+    for (let i = 1; i < 4; i++) {
+        document.getElementById(`tag${i}`).classList.toggle('dark')
+        document.getElementById(`tagg${i}`).classList.toggle('dark')
+        document.getElementById(`taggg${i}`).classList.toggle('dark')
+    }
+    var gameModeSection = document.getElementsByClassName('gameplay')
+    for (let i = 0; i < 1; i++) {
+        document.getElementById(`gameMode${i}`).classList.toggle('dark')
+    }
+
+    newsSection.classList.toggle('dark');
+    section1.classList.toggle('dark');
+    learnMore.classList.toggle('dark');
+    h3.classList.toggle('dark');
+    gameModeSection.classList.toggle('dark');
+    gameModeSectionh2.classList.toggle('dark');
+    gameModeSectionh1.classList.toggle('dark');
+    gameMode.classList.toggle("dark");
+    card.classList.toggle("dark");
+    element.classList.toggle("dark");
+}
